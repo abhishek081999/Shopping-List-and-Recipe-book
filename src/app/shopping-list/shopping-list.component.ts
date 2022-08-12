0565be7cbb4a8df9c1c08a5ef617a959be597a20
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-08-12 17:13:38 +0530

    Course project- The basics  12/08/2022

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingerdient } from '../shared/ingedient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients:Ingerdient[] = [
+    new Ingerdient('Banana',5),
+    new Ingerdient('Tomato',9),
+    new Ingerdient('Apple',4)
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
